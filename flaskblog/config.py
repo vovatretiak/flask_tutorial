7d9c08012bb27810bce538b4f237dd0dc5aaaf72
Vova <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>
2022-07-18 13:10:39 +0300

    learning flask framework

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '41527344b4d70334dfcb77962b7a9bca'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '------@gmail.com'
+    MAIL_PASSWORD = '------'
